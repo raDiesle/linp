4067e11fc4f899059fb1b3578e7f8dfa5b900bac
Lina Asadullina <l.i.asadullina@gmail.com>
2017-08-27 16:43:29 +0200

    cards for database added

--- linp/src/app/simulation/linpcards.model.ts
+++ linp/src/app/simulation/linpcards.model.ts
@@ -11,15 +11,20 @@ export class LinpCardsModelService {
       "Plastik", "Kamera", "Spiel", "Nacht", "Dieb", "Ruder", "Aktie", "Salbe", "Warze", "Klippe", "Peitsche", "Dresden",
       "Winter", "Stock", "Gärtner", "Bohne", "Löwe", "Flur", "Kraftwerk","Pfanne", "Schlauch", "Viertel", "Kiste", "Brille",
       "Stamm", "Kino", "Bande", "Schlüssel", "Gruppe", "Hafen","Kamerad", "Sprung", "Tritt", "Hurrikan", "Schlag", "Köln",
-      "Wind", "Mikrofon", "Mann", "Becher", "Alkohol", "Treppe",
-      "Land", "Natur", "Lunge", "Gangway", "Stahl", "Ägypten", "Feder", "Bogen", "Kopie", "Tasche", "Wolf",
-      "Wiese", "Zar", "Ostern", "Stille", "Konzert", "Mund", "San Francisco", "Markt", "Kasten", "Kind",
-      "Uhr", "Salz", "Garten", "Manchester", "Nelke", "Mähne", "Gebet", "Sack", "Hof",
-      "Ufer", "Sport", "Mehl", "Silvester", "Schnabel","Rakete", "Redner", "Terrine", "Beil", "Linse",
-      "Klappe", "Absatz"
-      ,"Erde","Note", "Fahne", "Nagel", "Esel", "Schuppen","New York", "Rose", "Porträt", "Geschoß", "Schlaf", "Australien",
+      "Wind", "Mikrofon", "Mann", "Becher", "Alkohol", "Treppe", "Land", "Natur", "Lunge", "Gangway", "Stahl", "Ägypten",
+      "Feder", "Bogen", "Kopie", "Tasche", "Wolf", "Wiese", "Zar", "Ostern", "Stille", "Konzert", "Mund", "San Francisco",
+      "Ufer", "Sport", "Mehl", "Silvester", "Schnabel","Rakete", "Redner", "Terrine", "Beil", "Linse", "Klappe", "Absatz",
+      "Markt", "Kasten", "Kind", "Uhr", "Salz", "Garten", "Manchester", "Nelke", "Mähne", "Gebet", "Sack", "Hof",
+      "Erde","Note", "Fahne", "Nagel", "Esel", "Schuppen","New York", "Rose", "Porträt", "Geschoß", "Schlaf", "Australien",
       "Strand", "Spieler", "Bauer", "Weihnachten", "Hahn","Dach","Präsident", "Werkzeug","Kies", "Lanze", "Hals", "Jerusalem",
-      "IMG_1876"
+      "Sonne", "Prinzessin","Schlachter", "Brot", "Hund", "Knopf", "Camping", "Plombe", "Sarg", "Kamm", "Rücken", "Hemd",
+      "Wald", "Radio", "Kreide", "Topf", "Zucker", "Haus", "Ingenieur", "Tatze", "Tennis", "Hügel", "Leder", "Kairo",
+      "Gold", "Gitarre", "Schwester", "Bürger", "Gas", "Stollen", "Blüte", "Faust", "Dolch", "Brust", "Europa",
+      "Höhle", "Papier", "Bauch", "Hälfte", "Stachel", "Skateboard", "Schloss", "Pudding", "Staubsauger", "Matratze", "Schrank", "Fluss",
+      "Küste", "Ball", "Trecker", "Kalender", "Haar", "Drachen", "Professor", "Welle", "Semester", "Leiste", "Zahn", "Wien",
+
+
+
     ]
   }
 
